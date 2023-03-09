0232a74942c6789521c6486b76be99b2c273d1e7
paolasensato <sensatopaola@gmail.com>
2023-03-09 12:07:43 -0300

    Implementação das funções de começar e pausar contador

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+const progressBarNumber = document.querySelector('#progressBar .progress-value');
+const buttonTypePomodoro = document.querySelector('#buttonTypePomodoro');
+
+const audio = new Audio('./sounds/alarm.mp3');
+
+const pomodoroEmSegundos = 60 * 1;
+const TYPE_POMODORO = "POMODORO";
+
+let progressInterval;
+let pomodoroTipo = TYPE_POMODORO;
+let valorTimer = pomodoroEmSegundos;
+
+function formataNumeroEmString(number) {
+
+    const minutos = Math.trunc(number / 60)
+        .toString()
+        .padStart(2, '0');
+    const segundos = Math.trunc(number % 60)
+        .toString()
+        .padStart(2, '0');
+
+    return `${minutos}:${segundos}`;
+}
+
+const comecarTemporizador = () => {
+    progressInterval = setInterval(() => {
+        valorTimer = valorTimer - 1;
+        setProgressoTemporizador();
+    }, 1000);
+    
+}
+
+const pararTemporizador = () => clearInterval(progressInterval);
+
+function setProgressoTemporizador() {
+
+    if(valorTimer === 0) {
+        pararTemporizador();
+        audio.play();
+    }
+
+    progressBarNumber.textContent = `${formataNumeroEmString(valorTimer)}`;
+
+}
